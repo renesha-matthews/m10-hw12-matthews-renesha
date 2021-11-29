45aa0a89910c960d8623231ab3c24ad6eed5767e
Renesha Matthews <reneshamatthews9@gmail.com>
2021-11-29 15:29:44 -0500

    Add Car class and its methods to index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// YOUR CODE HERE
+class Car {
+    constructor(make, model, year) {
+        this.make = make
+        this.model = model
+        this.year = year
+    }
+
+    honk() {
+        console.log("BEEP BEEP!")
+    }
+
+    performMaintenance() {
+        setTimeout(function() {
+            console.log('maintenance complete')
+        }, 3000)
+    }
+}
